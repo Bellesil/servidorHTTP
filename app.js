f00b2a91d3e269d2170422a3fe215384de70211b
Bellesil <isabellerochasil55@gmail.com>
2024-09-16 10:18:26 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const smile = express();
+
+const port = 3002;
+
+
+smile.use(express.static(path.join(__dirname, 'public')));
+
+smile.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public' ,'index.html'));
+});
+
+smile.listen(port, () => {
+    console.log(`Servidor rodando em http://127.0.0.1:${port}`);
+});
